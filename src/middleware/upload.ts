868719342f0a9292be73a2bd2374999c03cb8c62
Nitin <adityagithub311@gmail.com>
2026-06-17 19:19:36 +0530

    variant

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = "uploads/company";
+const uploadDir = "uploads";
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
